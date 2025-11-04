6c3027282d85ed3bdafe6d2433c27e2a20b09340
MattHeeper <MattHeeper@gmail.com>
2025-11-04 22:04:24 +0330

    new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8") if (HERE / "
 
 setup(
     name="devion",                     # اسم پکیجی که میخواهی در PyPI منتشر کنی
-    version="0.1.6",
+    version="0.1.7",
     description="Devion — Development Environment Manager (core python package)",
     long_description=long_description,
     long_description_content_type="text/markdown",
